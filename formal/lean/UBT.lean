b61f0be6609e42e3773ce9b0252cfa07f5795e0d
David Jaros <jdavid.cz@gmail.com>
2026-09-16 10:45:24 +0200

    Formalize corrected chiral currents and winding covariance with interactions

--- formal/lean/UBT.lean
+++ formal/lean/UBT.lean
@@ -9,3 +9,4 @@ import UBT.GR.VolumePrincipalSymbol
 import UBT.Action.PotentialVacuum
 import UBT.Action.PotentialMinimumOrbit
 import UBT.Action.PsiRestHamiltonian
+import UBT.Action.ChiralInteraction
